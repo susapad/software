e37d3f8ce3e53cc2eddbed96c9318d54e70cb7e6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-21 15:18:28 -0300

    rename `susa.py` to `__main__.py`

--- susapad/__main__.py
+++ susapad/__main__.py
@@ -4,12 +4,12 @@ import sys
 
 from PySide6 import QtWidgets
 
-from susa.ui import main_window
+from susapad.susa.ui import main_window
 
 
 def run():
     """Start SusaPad's application"""
-    
+
     susa_app = QtWidgets.QApplication([])
     susa_window = main_window.MainWindow()
     susa_window.resize(300, 500)
