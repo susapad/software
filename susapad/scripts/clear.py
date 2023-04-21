82f3b9fd627a6e568e282d7f183adbe5eca7849a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-21 11:57:48 -0300

    add documentation

--- susapad/scripts/clear.py
+++ susapad/scripts/clear.py
@@ -1,7 +1,15 @@
+""" Clean __pycache__'s folders
+
+To use this module simply run `poetry run clean`.
+
+But, you can also `python susapad/scripts/clean` for Linux
+and `py .\\susapad\\scripts\\clear.py` for Windows as well.
+"""
 
 import pathlib as path
 
 class PyCache:
+    """"Stores and cleans the project's __pycache__ folders"""
 
     def __init__(self, root_dir: str):
         self.directories: list[path.Path] = \
@@ -17,14 +25,18 @@ class PyCache:
             file.unlink()
 
     def clear_pycache(self):
+        """cleans `self.directories`'s folders and files"""
         for directory in self.directories:
             self._delete_pyc_files(directory)
             directory.rmdir()
 
 
-# == Callable == 
+# == Callable ==
 
 def run():
+    """"Starts the script 
+    - use your Current Working Directory as root path
+    """
     print("Clearing __pycache__...")
     PyCache(".").clear_pycache()
 
