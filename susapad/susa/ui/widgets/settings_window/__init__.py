3f8aac5c50a30c02e1bb7eec09365f276d7672b9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-25 20:04:32 -0300

    add items to layout

--- susapad/susa/ui/widgets/settings_window/__init__.py
+++ susapad/susa/ui/widgets/settings_window/__init__.py
@@ -1,7 +1,7 @@
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
 
-from . import rapid_trigger
+from . import actuation_point, togglers, sensibility
 
 
 class FormsGroup(QtWidgets.QWidget):
@@ -9,11 +9,26 @@ class FormsGroup(QtWidgets.QWidget):
     def __init__(self, window, susapad):
         super().__init__()
 
-        self.activate_toggle = rapid_trigger\
-            .RapidTriggerButton(window, susapad)
+        self.rt_button = togglers.RapidTriggerButton(window, susapad)
+        self.crt_button = togglers.ContinuousRapidTriggerButton(window, susapad)
+
+        self.sensibility_slider_press = sensibility.PressSensibilitySlider(window, susapad)
+        self.sensibility_slider_release = sensibility.ReleaseSensibilitySlider(window, susapad)
+        
+        self.actuation_slider_lower = actuation_point.LowerActuationSlider(window, susapad, self)
+        self.actuation_slider_upper = actuation_point.UpperActuationSlider(window, susapad, self)
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.activate_toggle)
+        self.layout.addWidget(QtWidgets.QLabel("Rapid Trigger"))
+        self.layout.addWidget(self.rt_button)
+        self.layout.addWidget(QtWidgets.QLabel("Rapid Trigger Contínuor"))
+        self.layout.addWidget(self.crt_button)
+        self.layout.addWidget(QtWidgets.QLabel("Sensibilidade: Pressionar e Soltar"))
+        self.layout.addWidget(self.sensibility_slider_press, alignment = Qt.AlignJustify)
+        self.layout.addWidget(self.sensibility_slider_release, alignment = Qt.AlignJustify)
+        self.layout.addWidget(QtWidgets.QLabel("Pontos de Ativação: Mínimo e Máximo"))
+        self.layout.addWidget(self.actuation_slider_lower, alignment = Qt.AlignJustify)
+        self.layout.addWidget(self.actuation_slider_upper, alignment = Qt.AlignJustify)
 
 
 
