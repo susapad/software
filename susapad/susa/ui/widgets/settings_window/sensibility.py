a41c2400ea493793a505105bdcf5478963687447
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-25 18:39:44 -0300

    create sensibility control

---
+++ susapad/susa/ui/widgets/settings_window/sensibility.py
@@ -0,0 +1,39 @@
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import Qt
+
+
+
+class PressSensibilitySlider(QtWidgets.QSlider):
+
+    def __init__(self, window, susapad):
+        super().__init__()
+
+        self.susapad = susapad
+
+        self.setMinimum(10)
+        self.setMaximum(400)
+        self.setOrientation = Qt.Horizontal
+
+        self.sliderReleased.connect(self.action)
+
+    @QtCore.Slot()
+    def action(self):
+        self.susapad.set_press_sensibility(self.value)
+
+
+class ReleaseSensibilitySlider(QtWidgets.QSlider):
+
+    def __init__(self, window, susapad):
+        super().__init__()
+
+        self.susapad = susapad
+
+        self.setMinimum(10)
+        self.setMaximum(400)
+        self.setOrientation = Qt.Horizontal
+
+        self.sliderReleased.connect(self.action)
+
+    @QtCore.Slot()
+    def action(self):
+        self.susapad.set_release_sensibility(self.value)
