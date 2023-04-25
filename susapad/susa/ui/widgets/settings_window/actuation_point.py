9ea5a14230242c7fe2e2bb31e99e4e5049605be1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-25 20:02:11 -0300

    add Actuation Point configurator sliders

---
+++ susapad/susa/ui/widgets/settings_window/actuation_point.py
@@ -0,0 +1,47 @@
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import Qt
+
+
+
+class LowerActuationSlider(QtWidgets.QSlider):
+
+    def __init__(self, window, susapad, forms):
+        super().__init__()
+
+        self.susapad = susapad
+
+        self.setMinimum(10)
+        self.setMaximum(400)
+        self.setOrientation(Qt.Horizontal)
+        self.setMinimumWidth(400)
+
+
+        self.sliderReleased.connect(self.action)
+
+    @QtCore.Slot()
+    def action(self):
+        self.susapad.set_actuation_point_lower(self.value())
+        self.forms.actuation_slider_upper.setMinimum(self.value())
+        self.forms.actuation_slider_upper.action()
+
+
+class UpperActuationSlider(QtWidgets.QSlider):
+
+    def __init__(self, window, susapad, forms):
+        super().__init__()
+
+        self.susapad = susapad
+        self.forms = forms
+
+        self.setMinimum(10)
+        self.setMaximum(400)
+        self.setOrientation(Qt.Horizontal)
+        self.setMinimumWidth(400)
+
+        self.sliderReleased.connect(self.action)
+
+    @QtCore.Slot()
+    def action(self):
+        self.susapad.set_actuation_point_upper(self.value())
+        self.forms.actuation_slider_lower.setMaximum(self.value())
+        self.forms.actuation_slider_lower.action()
