c6ecb66d22034929b0fa903db0aff98012160e9f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-25 10:32:14 -0300

    create `BaseWindow` to have an uniform window

---
+++ susapad/susa/ui/widgets/common/window.py
@@ -0,0 +1,39 @@
+from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6.QtCore import Qt
+
+
+class BaseWindow(QtWidgets.QWidget):
+
+    def __init__(self, susapad, parent = None):
+
+        super().__init__()
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self._configure_shadows()
+        self.resize(500, 200)
+
+        self.susapad = susapad
+        self.oldPos = None
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        
+    
+    def _configure_shadows(self):
+        shadow = QtWidgets.QGraphicsDropShadowEffect(self)
+        shadow.setOffset(QtCore.QPoint(0,0))
+        shadow.setBlurRadius(30)
+        shadow.setColor(QtGui.QColor(0,0,0))
+        self.setGraphicsEffect(shadow)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        if self.oldPos is not None:
+            delta = event.globalPos() - self.oldPos
+            self.move(self.pos() + delta)
+            self.oldPos = event.globalPos()
+
+    def mouseReleaseEvent(self, event):
+        self.oldPos = None
