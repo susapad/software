d7f9108fac47d491b45bddc80dadc2c6d458d863
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-25 22:44:25 -0300

    add StyleSheet to `BaseButton`

--- susapad/susa/ui/widgets/common/button.py
+++ susapad/susa/ui/widgets/common/button.py
@@ -14,3 +14,35 @@ class BaseButton(QtWidgets.QPushButton):
 
     def _init_style(self):
         self.setFixedSize(100, 40)
+        self.setStyleSheet(
+            """
+                QPushButton {
+                    background-color: #0e639e;
+                    border-radius: 15px;
+                    min-width: 10em;
+                    padding: 6px;
+                    font: bold;
+                    color: white;
+                }
+
+                QPushButton:hover {
+                    background-color: #127ecb;
+                }
+
+                QPushButton:pressed {
+                    background-color: #0a4874;
+                }
+
+                QPushButton[accessibleName="secondary"] {
+                    background-color: #b71970;
+                }
+
+                QPushButton:hover[accessibleName="secondary"] {
+                    background-color: #dd1e87;
+                }
+
+                QPushButton:pressed[accessibleName="secondary"] {
+                    background-color: #861252;
+                }
+            """
+        )
