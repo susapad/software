d952e5e948f76c3dfb2a45b00291a3a449c936b3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-29 17:06:27 -0300

    refactor `BaseButton`

--- susapad/base_widgets/button.py
+++ susapad/base_widgets/button.py
@@ -3,46 +3,47 @@ from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 
 
+_BUTTON_STYLE = """
+    QPushButton {
+        background-color: #0e639e;
+        border-radius: 15px;
+        min-width: 10em;
+        padding: 6px;
+        font: bold;
+        color: white;
+    }
+
+    QPushButton:hover {
+        background-color: #127ecb;
+    }
+
+    QPushButton:pressed {
+        background-color: #0a4874;
+    }
+
+    QPushButton[accessibleName="secondary"] {
+        background-color: #b71970;
+    }
+
+    QPushButton:hover[accessibleName="secondary"] {
+        background-color: #dd1e87;
+    }
+
+    QPushButton:pressed[accessibleName="secondary"] {
+        background-color: #861252;
+    }
+"""
+
+
 class BaseButton(QtWidgets.QPushButton):
 
     def __init__(self, text: str, shortcut: str = None, parent: str = None):
         super().__init__(text, parent)
-        self._init_style()
+
+        self.setFixedSize(100, 40)
+        self.setStyleSheet(_BUTTON_STYLE)
+        self.setCursor(Qt.PointingHandCursor)
+
         if shortcut:
             self.setShortcut(shortcut)
-            self.setCursor(Qt.PointingHandCursor)
-
-    def _init_style(self):
-        self.setFixedSize(100, 40)
-        self.setStyleSheet(
-            """
-                QPushButton {
-                    background-color: #0e639e;
-                    border-radius: 15px;
-                    min-width: 10em;
-                    padding: 6px;
-                    font: bold;
-                    color: white;
-                }
-
-                QPushButton:hover {
-                    background-color: #127ecb;
-                }
-
-                QPushButton:pressed {
-                    background-color: #0a4874;
-                }
-
-                QPushButton[accessibleName="secondary"] {
-                    background-color: #b71970;
-                }
-
-                QPushButton:hover[accessibleName="secondary"] {
-                    background-color: #dd1e87;
-                }
-
-                QPushButton:pressed[accessibleName="secondary"] {
-                    background-color: #861252;
-                }
-            """
-        )
+        
