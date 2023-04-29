05f814dfafe9f60f2a551b2ec33a3dcd51a79ff7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-29 19:54:53 -0300

    clean module's code - remove old `FormsGroup` - put qss outside the class

--- susapad/windows/settings/widgets/__init__.py
+++ susapad/windows/settings/widgets/__init__.py
@@ -4,11 +4,40 @@ from PySide6.QtCore import Qt
 from . import actuation_point, togglers, sensibility
 from .close import CloseButton
 
+_SETTINGS_STYLE = """
+    QFrame {
+        border-radius: 20px;
+        background-color: #121212;
+    }
 
-class FormsGroup(QtWidgets.QWidget):
+    QLabel {
+        color: white;
+        font: 16px;
+        margin-top: 20px;
+        margin-bottom: 10px;
+    } 
+
+    QLabel#first-h2 {
+        margin-top: 0px;
+    } 
+
+    QPushButton {
+        margin-left: 20px;
+    }
+"""
+
+
+class WindowLayout(QtWidgets.QFrame):
 
     def __init__(self, window, susapad):
         super().__init__()
+
+        # Configure Style
+
+        self.setStyleSheet(_SETTINGS_STYLE)
+        self.setContentsMargins(20, 20, 20, 20)
+
+        # Configure Layout
 
         self.first_h2 = QtWidgets.QLabel("Rapid Trigger")
         self.first_h2.setObjectName("first-h2")
@@ -18,7 +47,7 @@ class FormsGroup(QtWidgets.QWidget):
 
         self.sensibility_slider = sensibility.SensiblitySlidersGroup(window, susapad)
         self.actuation_slider = actuation_point.ActuationPointGroup(window, susapad)
-
+        
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.first_h2)
         self.layout.addWidget(self.rt_button)
@@ -26,47 +55,3 @@ class FormsGroup(QtWidgets.QWidget):
         self.layout.addWidget(self.crt_button)
         self.layout.addWidget(self.sensibility_slider)
         self.layout.addWidget(self.actuation_slider)
-
-
-
-class WindowLayout(QtWidgets.QFrame):
-
-    def __init__(self, window, susapad):
-        super().__init__()
-
-        self.setObjectName("background-frame")
-        self.__init_style()
-
-        self.forms = FormsGroup(window, susapad)
-        
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.forms)
-
-
-    def __init_style(self):
-        self.setStyleSheet(
-            """
-
-            QFrame {
-                border-radius: 20px;
-                background-color: #121212;
-            }
-
-            QLabel {
-                color: white;
-                font: 16px;
-                margin-top: 20px;
-                margin-bottom: 10px;
-            } 
-
-            QLabel#first-h2 {
-                margin-top: 0px;
-            } 
-
-            QPushButton {
-                margin-left: 20px;
-            }
-
-            """
-        )
-        self.setContentsMargins(20, 20, 20, 20)
