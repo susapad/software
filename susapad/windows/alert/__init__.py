5b59bc0fe8ad3d8578e0161aa329b1c1d219ecdf
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-30 19:28:01 -0300

    implement new Alert Dialog

--- susapad/windows/alert/__init__.py
+++ susapad/windows/alert/__init__.py
@@ -1,26 +1,57 @@
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtCore import Qt
+
+from susapad import base_widgets as base
 
 
-class AlertDialog(QtWidgets.QDialog):
+_DEFAULT_MESSAGE = """SusaPad não encontrado. 
+Certifique-se que ele está conectado corretamente
+"""
 
-    def __init__(self, parent, message: str = None):
-        super().__init__(parent)
+class CloseButton(base.BaseButton):
 
-        self.message = message
-        if not self.message:
-            self.message = \
-                """
-                SusaPad não encontrado. 
-                Certifique-se que ele está conectado corretamente
-                """
+    def __init__(self, dialog, parent):
+        super().__init__("Ok", "Enter")
+
+        self.dialog = dialog
+        self.parent = parent
+
+        self.setAccessibleName("secondary")
+        self.clicked.connect(self.close_dialog)
+
+    @QtCore.Slot()
+    def close_dialog(self):
+        self.parent.setEnabled(True)
+        self.dialog.close()
 
 
-        self.setWindowTitle("Aviso")
-        self.button_box = QtWidgets.QDialogButtonBox(
-            QtWidgets.QDialogButtonBox.Ok)
-        self.button_box.accepted.connect(self.accept)
+class AlertFrame(base.BaseFrame):
 
+    def __init__(self, dialog, parent, message: str):
+        super().__init__()
+
+        self.dialog = dialog
+        self.parent = parent
+
+        self.close = CloseButton(self.dialog, self.parent)
+        self.label = QtWidgets.QLabel(message)
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(QtWidgets.QLabel(self.message))
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.close)
+
+
+class AlertDialog(base.BaseWindow):
+
+    def __init__(self, parent, message: str = None, susapad = None):
+        super().__init__(susapad)
+
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+
+        self.parent = parent
+        self.message = message if message else _DEFAULT_MESSAGE
+
+        ## Configure Layout
+        self.main_widget = AlertFrame(self, self.parent, self.message)
+        self.layout.addWidget(self.main_widget)
