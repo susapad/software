afe8a63f432b4a5c5da5622438b478610bf42474
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-29 21:37:29 -0300

    create `BaseFrame`

--- susapad/base_widgets/__init__.py
+++ susapad/base_widgets/__init__.py
@@ -1,11 +1,12 @@
 from .window import BaseWindow
+from .frame import BaseFrame
 from .button import BaseButton
 from .floating import BaseFloatingButton
 from .toggle import BaseToggleButton
 from .slider import BaseSliderGroup, BaseDualSliderGroup
 
 __all__ = [
-    "BaseWindow", 
+    "BaseWindow", "BaseFrame", 
     "BaseButton", "BaseFloatingButton", "BaseToggleButton", 
     "BaseSliderGroup", "BaseDualSliderGroup"
 ]
---
+++ susapad/base_widgets/frame.py
@@ -0,0 +1,19 @@
+from PySide6 import QtWidgets
+
+_FRAME_STYLE = """
+    QFrame {
+        border-radius: 20px;
+        background-color: #121212;
+    }
+"""
+
+
+class BaseFrame(QtWidgets.QFrame):
+
+    def __init__(self):
+        super().__init__()
+
+        # Configuration
+        self.setObjectName("background-frame")
+        self.setStyleSheet(_FRAME_STYLE)
+        self.setContentsMargins(20, 20, 20, 20)
