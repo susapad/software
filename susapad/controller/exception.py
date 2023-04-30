23af0295d4ecc6adcaee165e29bab388dbb7febe
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-30 19:26:33 -0300

    fix module's indentation

--- susapad/controller/exception.py
+++ susapad/controller/exception.py
@@ -3,9 +3,10 @@ from susapad.windows import alert
 
 
 def susapad_not_found(window):
-        dialog = alert.AlertDialog(window)
-        dialog.show()
+    #window.setEnabled(False)
+    dialog = alert.AlertDialog(window)
+    dialog.show()
 
 
 def close_current_window(window):
-        window.close()
+    window.close()
