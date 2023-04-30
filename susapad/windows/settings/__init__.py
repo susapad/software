ffb457aa93d2381873be008cb016d9c36512fbae
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-29 22:19:28 -0300

    remove unused property

--- susapad/windows/settings/__init__.py
+++ susapad/windows/settings/__init__.py
@@ -11,11 +11,7 @@ from . import widgets
 class SettingsWindow(base.BaseWindow):
 
     def __init__(self, parent):
-
         super().__init__(parent.susapad, parent)
-
-        self.is_on: bool = True
-        self.rts: int = 200
 
         self.settings_widget = widgets.WindowLayout(self, self.susapad)
         self.layout.addWidget(self.settings_widget)
