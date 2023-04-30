b1898e4625a1549d1281531c23282fb2cb0b6556
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-29 21:26:10 -0300

    improve `CloseButton` code with `BaseFloatingButton`

--- susapad/windows/settings/widgets/close.py
+++ susapad/windows/settings/widgets/close.py
@@ -1,29 +1,16 @@
 
+from PySide6 import QtCore
+
 from susapad import base_widgets as base
 
 
-class CloseButton(base.BaseButton):
+class CloseButton(base.BaseFloatingButton):
 
     def __init__(self, window, main_window):
-        super().__init__("Fechar", "Escape", window)
+        super().__init__(window, "Fechar", "Escape")
 
-        self.setAccessibleName("close")
-        #self.setFixedSize(30, 30)
-        self.setGeometry(420, 20, 60, 16)
-        self.clicked.connect(main_window.close_settings_window)
-        self.setStyleSheet(
-            """
-            QPushButton[accessibleName="close"] {
-                    background-color: #b71970;
-                    border-radius: 10px;
-                    min-width: 60px;
-                    max-width: 60px;
-                    font: bold;
-                    color: white;
-                }
+        self.main_window = main_window
 
-            QPushButton:hover[accessibleName="close"] {
-                background-color: #dd1e87;
-            }
-            """
-        )
+    @QtCore.Slot()
+    def action(self):
+        self.main_window.close_settings_window()
