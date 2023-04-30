17271e47eb89b830128753ed856dce08441cacd2
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-04-29 21:25:40 -0300

    create `BaseFloatingButton`

--- susapad/base_widgets/__init__.py
+++ susapad/base_widgets/__init__.py
@@ -1,9 +1,11 @@
-from .button import BaseButton
 from .window import BaseWindow
+from .button import BaseButton
+from .floating import BaseFloatingButton
 from .toggle import BaseToggleButton
 from .slider import BaseSliderGroup, BaseDualSliderGroup
 
 __all__ = [
-    "BaseButton", "BaseWindow", "BaseToggleButton", 
+    "BaseWindow", 
+    "BaseButton", "BaseFloatingButton", "BaseToggleButton", 
     "BaseSliderGroup", "BaseDualSliderGroup"
 ]
---
+++ susapad/base_widgets/floating.py
@@ -0,0 +1,47 @@
+
+
+from . import button as base
+
+
+_FLOATING_STYLE = """
+    QPushButton[accessibleName="floating"] {
+        background-color: #b71970;
+        border-radius: 10px;
+        min-width: 60px;
+        max-width: 60px;
+        font: bold;
+        color: white;
+    }
+
+    QPushButton:hover[accessibleName="floating"] {
+        background-color: #dd1e87;
+    }
+"""
+
+
+class BaseFloatingButton(base.BaseButton):
+
+    def __init__(self, window, text, shortcut: str = None):
+        super().__init__(text, parent=window, shortcut=shortcut)
+
+        self.window = window
+
+        self.width = 60
+        self.height = 16
+        self.margin = 20
+
+        self.setAccessibleName("floating")
+        self.clicked.connect(self.action)
+        self.setStyleSheet(_FLOATING_STYLE)
+        self._set_position()
+
+    def action(self):
+        pass
+
+    def _set_position(self):
+        self.setGeometry(
+            self.window.width() - self.width,
+            self.margin,
+            self.width,
+            self.height
+        )
