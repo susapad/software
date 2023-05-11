789bcc3435de81b78d1001ee6f05d1cec1ad35c6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-11 15:16:56 -0300

    divide `AlertFrame` into multiple methods

--- susapad/window/alert/ui.py
+++ susapad/window/alert/ui.py
@@ -23,14 +23,19 @@ class AlertFrame(base_widgets.BaseFrame):
 
     def __init__(self, dialog, parent, message: str):
         super().__init__()
-
-
+        self.message = message
         self.dialog = dialog
         self.parent = parent
 
-        self.close = CloseButton(self.dialog, self.parent)
-        self.label = QtWidgets.QLabel(message)
+        self.init_widgets()
+        self.init_layout()
 
+
+    def init_widgets(self)
+        self.close_button = CloseButton(self.dialog, self.parent)
+        self.content = QtWidgets.QLabel(self.message)
+
+    def init_layout(self):
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.close)
