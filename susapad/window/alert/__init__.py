9ba1ad05f5a9daa4a47e6f128b1a7c878ac25584
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-11 15:28:49 -0300

    reduce complexity

--- susapad/window/alert/__init__.py
+++ susapad/window/alert/__init__.py
@@ -14,14 +14,25 @@ Certifique-se que ele está conectado corretamente
 
 class AlertDialog(base.BaseWindow):
 
-    def __init__(self, parent, message: str = None, susapad = None):
+    def __init__(self, caller, message: str = None, susapad = None):
         super().__init__(susapad)
-
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-
-        self.parent = parent
+        self.caller = caller
         self.message = message if message else _DEFAULT_MESSAGE
 
-        ## Configure Layout
-        self.main_widget = ui.AlertUI(self, self.parent, self.message)
+        self.init_proprieties()
+        self.init_widgets()
+        self.init_layout()
+
+    def init_proprieties(self):
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+
+    def init_widgets(self):
+        self.main_widget = ui.AlertUI(self, self.message)
+
+    def init_layout(self):
         self.layout.addWidget(self.main_widget)
+
+    @QtCore.Slot()
+    def close_dialog(self):
+        self.caller.setEnabled(True)
+        self.close()
--- susapad/window/alert/ui.py
+++ susapad/window/alert/ui.py
@@ -4,32 +4,24 @@ from susapad import base_widgets
 
 class AlertUI(base_widgets.BaseFrame):
 
-    def __init__(self, dialog, caller, message: str):
+    def __init__(self, dialog, message: str):
         super().__init__()
-        self.message = message
         self.dialog = dialog
-        self.caller = caller
 
-        self.init_widgets()
+        self.init_widgets(message)
         self.init_layout()
 
     def init_close_button(self) -> base_widgets.BaseButton:
         button = base_widgets.BaseButton("Ok", "Enter")
         button.setAccessibleName("secondary")
-        button.clicked.connect(self.close_dialog)
+        button.clicked.connect(self.dialog.close_dialog)
         return button
 
     def init_widgets(self)
         self.close_button = self.init_close_button()
-        self.content = QtWidgets.QLabel(self.message)
+        self.content = QtWidgets.QLabel(message)
 
     def init_layout(self):
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.close)
-
-
-    @QtCore.Slot()
-    def close_dialog(self):
-        self.caller.setEnabled(True)
-        self.dialog.close()
