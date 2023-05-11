3d527179a69304338d7ad8201565848e21aff880
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-11 15:20:08 -0300

    turn `CloseButton` into a small function

--- susapad/window/alert/ui.py
+++ susapad/window/alert/ui.py
@@ -2,23 +2,6 @@
 from susapad import base_widgets
 
 
-class CloseButton(base_widgets.BaseButton):
-
-    def __init__(self, dialog, parent):
-        super().__init__("Ok", "Enter")
-
-        self.dialog = dialog
-        self.parent = parent
-
-        self.setAccessibleName("secondary")
-        self.clicked.connect(self.close_dialog)
-
-    @QtCore.Slot()
-    def close_dialog(self):
-        self.parent.setEnabled(True)
-        self.dialog.close()
-
-
 class AlertFrame(base_widgets.BaseFrame):
 
     def __init__(self, dialog, parent, message: str):
@@ -30,12 +13,23 @@ class AlertFrame(base_widgets.BaseFrame):
         self.init_widgets()
         self.init_layout()
 
+    def init_close_button(self) -> base_widgets.BaseButton:
+        button = base_widgets.BaseButton("Ok", "Enter")
+        button.setAccessibleName("secondary")
+        button.clicked.connect(self.close_dialog)
+        return button
 
     def init_widgets(self)
-        self.close_button = CloseButton(self.dialog, self.parent)
+        self.close_button = self.init_close_button()
         self.content = QtWidgets.QLabel(self.message)
 
     def init_layout(self):
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.close)
+
+
+    @QtCore.Slot()
+    def close_dialog(self):
+        self.parent.setEnabled(True)
+        self.dialog.close()
