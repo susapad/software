53633c6884879acb9cd268c6bec135267d91ffa3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-11 19:29:01 -0300

    `Widget\window`: refactor to be more pythonic

--- susapad/widget/window.py
+++ susapad/widget/window.py
@@ -1,3 +1,6 @@
+from __feature__ import true_property
+from __feature__ import snake_case
+
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
 
@@ -7,34 +10,34 @@ class BaseWindow(QtWidgets.QWidget):
     def __init__(self, susapad, parent = None):
 
         super().__init__()
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.set_window_flags(Qt.FramelessWindowHint)
+        self.set_attribute(Qt.WA_TranslucentBackground)
         self._configure_shadows()
         self.resize(500, 200)
 
         self.parent = parent
         self.susapad = susapad
-        self.oldPos = None
+        self.old_pos = None
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        
-    
+
+
     def _configure_shadows(self):
         shadow = QtWidgets.QGraphicsDropShadowEffect(self)
-        shadow.setOffset(QtCore.QPoint(0,0))
-        shadow.setBlurRadius(30)
-        shadow.setColor(QtGui.QColor(0,0,0))
-        self.setGraphicsEffect(shadow)
+        shadow.offset = QtCore.QPoint(0,0)
+        shadow.blur_radius = 30
+        shadow.color = QtGui.QColor(0,0,0)
+        self.graphics_effect = shadow
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
-            self.oldPos = event.globalPos()
+            self.old_pos = event.global_pos()
 
     def mouseMoveEvent(self, event):
-        if self.oldPos is not None:
-            delta = event.globalPos() - self.oldPos
-            self.move(self.pos() + delta)
-            self.oldPos = event.globalPos()
+        if self.old_pos is not None:
+            delta = event.global_pos() - self.old_pos
+            self.move(self.pos + delta)
+            self.old_pos = event.global_pos()
 
     def mouseReleaseEvent(self, event):
-        self.oldPos = None
+        self.old_pos = None
