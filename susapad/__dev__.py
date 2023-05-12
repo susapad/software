603b9cfb4d80c27410e06a47166303b2be2f4ac3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2023-05-12 19:24:27 -0300

    add alternative ways to run without susapad for developers test the implementation

---
+++ susapad/__dev__.py
@@ -0,0 +1,36 @@
+"""SusaPad's main file"""
+
+import os, sys, pathlib
+
+sys.path.append(
+    os.path.dirname(
+        pathlib.Path(os.path.realpath(__file__))\
+        .parent.absolute()
+    )
+)
+
+from PySide6 import QtWidgets
+
+from susapad.windows import main
+from susapad.controller import susapad as susapad_module
+
+
+def debug():
+    """Start SusaPad's application"""
+
+    susapad = susapad_module.SusaPad(True)
+    susa_app = QtWidgets.QApplication([])
+    susa_window = main.MainWindow(susapad)
+    susa_window.show()
+    sys.exit(susa_app.exec())
+
+
+def debug_insider():
+    """Start SusaPad's application"""
+
+    susapad = susapad_module.SusaPad(True)
+    susapad.insider = True
+    susa_app = QtWidgets.QApplication([])
+    susa_window = main.MainWindow(susapad)
+    susa_window.show()
+    sys.exit(susa_app.exec())
